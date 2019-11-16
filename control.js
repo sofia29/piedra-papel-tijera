fe965b81e94a788922bcb8e3b3474f6d174b1be0
sofia29 <sofia29@gmail.com>
2019-11-16 19:56:09 +1100

    primer commit

---
+++ control.js
@@ -0,0 +1,59 @@
+//Piedra, papel o tijera
+
+let respuestaPrimerUsuario = prompt("Usuario 1: ¿Piedra, papel o tijera?")
+let ganadorUno = "Ganó el Usuario 1"
+let ganadorDos = "Ganó el Usuario 2"
+let sinGanador = "Empataron"
+let invalida = "No ingresaste una opción válida. Se acabó el juego "
+
+if (respuestaPrimerUsuario === "piedra") {
+    let respuestaSegundoUsuario = prompt("Usuario 2: ¿Piedra, papel o tijera?");
+    if (respuestaSegundoUsuario === "piedra") {
+        alert(sinGanador)
+    }
+    else if (respuestaSegundoUsuario === "papel") {
+        alert(ganadorDos)
+    }   
+    else if (respuestaSegundoUsuario === "tijera") {
+        alert(ganadorUno)
+    }
+    else {
+        alert("No ingresaste una opcion valida")
+    }
+}
+
+else if (respuestaPrimerUsuario === "tijera") {
+    let respuestaSegundoUsuario = prompt("Usuario 2: ¿Piedra, papel o tijera?");
+    if (respuestaSegundoUsuario === "piedra") {
+        alert(ganadorDos)
+    }
+    else if (respuestaSegundoUsuario === "papel") {
+        alert(ganadorUno)
+    }   
+    else if (respuestaSegundoUsuario === "tijera") {
+        alert(sinGanador)
+    }
+    else {
+        alert("No ingresaste una opcion valida")
+    }
+}
+
+else if (respuestaPrimerUsuario === "papel") {
+    let respuestaSegundoUsuario = prompt("Usuario 2: ¿Piedra, papel o tijera?");
+    if (respuestaSegundoUsuario === "piedra") {
+        alert(ganadorUno)
+    }
+    else if (respuestaSegundoUsuario === "papel") {
+        alert(sinGanador)
+    }   
+    else if (respuestaSegundoUsuario === "tijera") {
+        alert(ganadorDos)
+    }
+    else {
+        alert("No ingresaste una opcion valida")
+    }
+}
+
+else {
+    alert("No ingresaste una opcion valida")
+}
